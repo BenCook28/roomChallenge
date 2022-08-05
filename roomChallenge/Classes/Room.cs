498e77205e780964cd51b8099222744efe0f6f19
benthomascook28@gmail.com <benthomascook28@gmail.com>
2022-08-05 13:17:44 -0400

    built room class

---
+++ roomChallenge/Classes/Room.cs
@@ -0,0 +1,13 @@
+namespace Classes
+{
+    public class Room
+    {
+        public decimal roomLength { get; set; }
+        public decimal roomWidth { get; set; }
+        public decimal roomHeight { get; set; }
+    }
+    public void calculateArea(decimal length, decimal width, decimal heigth)
+    {
+
+    }
+}
---
+++ roomChallenge/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("This program calculates room area.");
+Console.WriteLine("What is the length?");
+
+int roomLength;
+roomLength = Console.ReadLine();
+
+Console.WriteLine("What is the width?");
